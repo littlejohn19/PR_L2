ff8368ab05ee763076a2319fa630a0cadc49533c
Kdabrowa6@gmail.com <5z4l3j@gmail.com>
2021-03-19 11:10:30 +0100

    Zrobiono zadanie 3 z lekcji 3

---
+++ navbar.js
@@ -0,0 +1,5 @@
+let nav = '<nav><a href="index.html">Home</a>&nbsp&nbsp' + '<a href="kontakt.html">Kontakt</a> </nav>'; 
+
+let navbarElem = document.querySelector(".navbar"); 
+
+navbarElem.innerHTML = nav;
